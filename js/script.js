ea754bdb00bdb5fc78f56b8c49d9b9eff4a7c51c
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-15 02:26:23 +0530

    Async Await error handling!

--- js/script.js
+++ js/script.js
@@ -6,29 +6,40 @@ function breathe(amount) {
   return new Promise((resolve, reject) => {
     if (amount < 500) {
       /* eslint-disable prefer-promise-reject-errors */
-      reject('Something went wrong!');
+      reject('That is too small of a value');
       /* eslint-enable */
     }
-    setTimeout(() => resolve(`Done for ${amount} ms`), amount);
+    setTimeout(() => resolve(`Completed after ${amount} ms`), amount);
   });
 }
 
-breathe(1000)
-  .then((response) => {
-    console.log(response);
-    return breathe(500);
-  })
-  .then((response) => {
-    console.log(response);
-    return breathe(600);
-  })
-  .then((response) => {
-    console.log(response);
-    return breathe(400);
-  })
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+function catchErrors(fn) {
+  return function (...args) {
+    return fn(...args).catch((error) => {
+      console.error(`Sorry but there is an error: ${error}`);
+    });
+  };
+}
+
+const go = async (name, last) => {
+  console.log(`${name} ${last} started breathing.`);
+  let res;
+
+  res = await breathe(1000);
+  console.log(`First Breathe: ${res}`);
+
+  res = await breathe(500);
+  console.log(`Second Breathe: ${res}`);
+
+  res = await breathe(700);
+  console.log(`Third Breathe: ${res}`);
+
+  res = await breathe(400);
+  console.log(`Fourth Breathe: ${res}`);
+
+  res = await breathe(900);
+  console.log(`Fifth Breathe: ${res}`);
+};
+
+const wrappedFunction = catchErrors(go);
+wrappedFunction('Harry', 'Manchanda');
